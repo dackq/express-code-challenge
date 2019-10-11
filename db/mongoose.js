3a6cd8a27fc540d725b966f36a98a2e2d32c4a60
dakkyd <dakkyed@gmail.com>
2019-10-11 18:29:55 +0300

    removed keep files

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
-const db = mongoose.connect(process.env.MONGODB_SERVER_URL, {
+mongoose.connect(process.env.MONGODB_SERVER_URL, {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useFindAndModify: false,
 	useUnifiedTopology: true
 });
-
-module.exports = db;
