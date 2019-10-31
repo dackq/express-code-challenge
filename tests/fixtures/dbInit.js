54bc54cfeb12137b32a60682c95290e11ae9409d
Dakota <dakotalreed@gmail.com>
2019-10-31 13:10:41 +0200

    exposed database control functions

--- tests/fixtures/dbInit.js
+++ tests/fixtures/dbInit.js
@@ -1,60 +1,7 @@
 require("dotenv").config();
 require("../../db/mongoose");
-const User = require("../../db/models/user");
-const Book = require("../../db/models/book");
-const Institution = require("../../db/models/institution");
+const { dbInit, createNewUser } = require("./dbController");
 const data = require("./testData.json");
 
-const clearDb = async () => {
-	await User.deleteMany();
-	await Institution.deleteMany();
-	await Book.deleteMany();
-};
-
-const createNewUser = async body => {
-	try {
-		const user = new User(body);
-		await user.save();
-	} catch (err) {
-		console.log(err);
-	}
-};
-
-const createNewInstitution = async body => {
-	try {
-		const institution = new Institution(body);
-		await institution.save();
-	} catch (err) {
-		console.log(err);
-	}
-};
-
-const createNewBook = async body => {
-	try {
-		const institution = await Institution.findOne({
-			name: body.owner
-		});
-		if (!institution._id) {
-			throw new Error("Unkown Institution");
-		}
-		body.owner = institution._id;
-		const book = new Book(body);
-		await book.save();
-	} catch (err) {
-		console.log(err);
-	}
-};
-
-const dbInit = async () => {
-	await clearDb();
-
-	for (let key of Object.keys(data.institutions)) {
-		await createNewInstitution(data.institutions[key]);
-	}
-
-	for (let key of Object.keys(data.books)) {
-		await createNewBook(data.books[key]);
-	}
-};
-
-dbInit();
+dbInit(data);
+createNewUser(data.users.preloadedUser);
