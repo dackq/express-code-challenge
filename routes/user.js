aaed089dcba142da737cac2d5ccd1f624653f98d
Dakota <dakotalreed@gmail.com>
2019-11-01 12:55:41 +0200

    changed comment describing sign in route

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const router = new express.Router();
 /* User Routes */
 
 // POST /users/signin
-// user credentials are authenticated by passport. If they are invalid, passport returns unauthorized. If they are valid this route returns an http reponse with a success message and the user data.
+// user credentials are authenticated by passport middleware. If they are invalid, passport returns unauthorized. If they are valid this route returns an http reponse with a success message and the user data.
 router.post(
 	"/users/signin",
 	passport.authenticate("local"),
